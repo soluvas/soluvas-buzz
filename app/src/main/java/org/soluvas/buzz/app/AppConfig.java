4da2234057b2d95c8ab1b46dd80ec8dc4aa609f7
Hendy Irawan <hendy@soluvas.com>
2013-08-08 03:39:27 +0700

    Yaay! App displays bootstrap blank page.

--- app/src/main/java/org/soluvas/buzz/app/AppConfig.java
+++ app/src/main/java/org/soluvas/buzz/app/AppConfig.java
@@ -1,8 +1,19 @@
 package org.soluvas.buzz.app;
 
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import com.google.common.eventbus.EventBus;
+import com.mchange.v2.c3p0.DataSources;
+import com.mchange.v2.c3p0.PooledDataSource;
 
 /**
  * @author ceefour
@@ -12,9 +23,24 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:/META-INF/buzz.properties")
 public class AppConfig {
 	
-//	@Bean
-//	public FrequencyJobApplication webApp() {
-//		return new FrequencyJobApplication();
-//	}
+	@Inject
+	private Environment env;
 	
+	@Bean
+	public BuzzApplication webApp() {
+		return new BuzzApplication();
+	}
+	
+	@Bean(destroyMethod="close")
+	public PooledDataSource dataSource() throws IllegalStateException, SQLException {
+		final DataSource unpooled = DataSources.unpooledDataSource(env.getRequiredProperty("jpaUrl"), 
+				env.getProperty("jpaUser"), env.getProperty("jpaPassword"));
+		return (PooledDataSource) DataSources.pooledDataSource(unpooled);
+	}
+	
+	@Bean
+	public EventBus eventBus() {
+		return new EventBus();
+	}
+
 }
--- app/src/main/java/org/soluvas/buzz/app/HomePage.java
+++ app/src/main/java/org/soluvas/buzz/app/HomePage.java
@@ -1,7 +1,40 @@
 package org.soluvas.buzz.app;
 
+import org.apache.wicket.extensions.breadcrumb.BreadCrumbBar;
+import org.apache.wicket.markup.head.filter.HeaderResponseContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.soluvas.web.bootstrap.GrowlBehavior;
+
+import de.agilecoders.wicket.core.markup.html.bootstrap.behavior.BootstrapBaseBehavior;
+import de.agilecoders.wicket.core.markup.html.bootstrap.html.HtmlTag;
+import de.agilecoders.wicket.core.markup.html.bootstrap.html.OptimizedMobileViewportMetaTag;
+import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.Navbar;
+import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.Navbar.Position;
 
 public class HomePage extends WebPage {
 
+	private static final long serialVersionUID = 1L;
+	private final Navbar navbar;
+
+	public HomePage(PageParameters parameters) {
+		super(parameters);
+
+		add(new HtmlTag("html"));
+        add(new OptimizedMobileViewportMetaTag("viewport"));
+        
+		add(new BootstrapBaseBehavior());
+		add(new GrowlBehavior(false));
+		add(new HeaderResponseContainer("footer-container", "footer-container"));
+		
+		navbar = new Navbar("navbar");
+		navbar.setOutputMarkupId(true);
+		navbar.setPosition(Position.TOP);
+		add(navbar.brandName(new Model<>("Buzz")));
+
+		final BreadCrumbBar breadCrumbBar = new BreadCrumbBar("breadCrumbBar");
+		add(breadCrumbBar);
+	}
+	
 }
